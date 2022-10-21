5930e355389dd02fdbc7d6cd269aa80c28ac8aed
VitorB2002 <vitorb.santos2002@gmail.com>
2022-10-21 11:11:19 -0300

    Add one round function

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,44 @@ function getComputerChoice(){
     return computerChoice;
 }
 
+function round(playerChoice, computerChoice){
+
+    switch (playerChoice){
+
+        case "rock":
+
+            if(computerChoice == "scissors"){
+                return "You Won! rock beats scissors";
+            }   else if(computerChoice == "paper"){
+                return "You Lose! paper beats rock"
+            }
+
+            break;
+
+        case "paper":
+
+            if(computerChoice == "rock"){
+                return "You Won! rock beats scissors";
+            }   else if(computerChoice == "scissors"){
+                return "You Lose! scissors beats paper"
+            }
+
+            break;
+
+        case "scissors":
+
+            if(computerChoice == "rock"){
+                return "You Lose! rock beats scissors";
+            }   else if(computerChoice == "scissors"){
+                return "You Won! scissors beats paper"
+            }
+
+            break;
+    }
+
+    return "It's a Draw, both players have choose " + playerChoice;
+}
+
 let playerChoice = prompt("Choose rock, paper or scissors: ").toLowerCase();
 
 while(playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors"){
